cdedaa3880e82444de6cbe599de6a4d6c33239cb
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2025-11-24 21:32:00 +0200

    Implemented changes: add a space at the beginning of the WHERE

--- app/managers.py
+++ app/managers.py
@@ -28,7 +28,7 @@ class ActorManager:
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
-            "SET first_name = ?, last_name = ?"
+            "SET first_name = ?, last_name = ? "
             "WHERE id = ?",
             (new_first_name, new_last_name, pk)
         )
